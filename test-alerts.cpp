4393c13a99f4c330a986215c558edfdc382fcc6f
SathishSivasekar <136423755+SathishSivasekar@users.noreply.github.com>
2023-06-24 21:12:53 +0530

    Update the classifyTemperatureBreah function condition

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -9,41 +9,41 @@
 
 /********    Temperature breach test for passive coolling type    **********/
 TEST_CASE("Test1 : Temperature breach for passive coolling type low limit") {
-  REQUIRE(inferBreach(PASSIVE_COOLING_TYPE, -5) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING_TYPE, -5) == TOO_LOW);
 }
 
 TEST_CASE("Test2 : Temperature breach for passive coolling type high limit") {
-  REQUIRE(inferBreach(PASSIVE_COOLING_TYPE, 40) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING_TYPE, 40) == TOO_HIGH);
 }
 
 TEST_CASE("Test3 : Temperature breach for passive coolling type high limit") {
-  REQUIRE(inferBreach(PASSIVE_COOLING_TYPE, 30) == NORMAL);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING_TYPE, 30) == NORMAL);
 }
 
 
 /********   Temperature breach test for high active coolling type   **********/
-TEST_CASE("Test4 : Temperature breach for high coolling type low limit") {
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING_TYPE, -5) == TOO_LOW);
+TEST_CASE("Test1 : Temperature breach for high coolling type low limit") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING_TYPE, -5) == TOO_LOW);
 }
 
-TEST_CASE("Test5 : Temperature breach for high coolling type high limit") {
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING_TYPE, 50) == TOO_HIGH);
+TEST_CASE("Test2 : Temperature breach for high coolling type high limit") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING_TYPE, 50) == TOO_HIGH);
 }
 
-TEST_CASE("Test6 : Temperature breach for high coolling type high limit") {
-  REQUIRE(inferBreach(HI_ACTIVE_COOLING_TYPE, 40) == NORMAL);
+TEST_CASE("Test3 : Temperature breach for high coolling type high limit") {
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING_TYPE, 40) == NORMAL);
 }
 
 
 /********   Temperature breach test for mid active coolling type   **********/
-TEST_CASE("Test7 : Temperature breach for mid coolling type low limit") {
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING_TYPE, -5) == TOO_LOW);
+TEST_CASE("Test1 : Temperature breach for mid coolling type low limit") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING_TYPE, -5) == TOO_LOW);
 }
 
-TEST_CASE("Test8 : Temperature breach for mid coolling type high limit") {
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING_TYPE, 45) == TOO_HIGH);
+TEST_CASE("Test2 : Temperature breach for mid coolling type high limit") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING_TYPE, 45) == TOO_HIGH);
 }
 
-TEST_CASE("Test9 : Temperature breach for mid coolling type high limit") {
-  REQUIRE(inferBreach(MED_ACTIVE_COOLING_TYPE, 35) == NORMAL);
+TEST_CASE("Test3 : Temperature breach for mid coolling type high limit") {
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING_TYPE, 35) == NORMAL);
 }
