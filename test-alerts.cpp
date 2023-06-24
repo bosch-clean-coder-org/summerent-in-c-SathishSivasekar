24402d975b1d8a2c43d9f212f997a8a0cfbb1215
SathishSivasekar <136423755+SathishSivasekar@users.noreply.github.com>
2023-06-24 13:57:05 +0530

    Start unit test for temperature breaching function

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -3,6 +3,47 @@
 #include "test/catch.hpp"
 #include "typewise-alert.h"
 
-TEST_CASE("infers the breach according to limits") {
-  REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
+#define PASSIVE_COOLING_TYPE    0
+#define HI_ACTIVE_COOLING_TYPE  1
+#define MED_ACTIVE_COOLING_TYPE 2
+
+/********    Temperature breach test for passive coolling type    **********/
+TEST_CASE("Test1 : Temperature breach for passive coolling type low limit") {
+  REQUIRE(inferBreach(PASSIVE_COOLING_TYPE, -5) == TOO_LOW);
+}
+
+TEST_CASE("Test2 : Temperature breach for passive coolling type high limit") {
+  REQUIRE(inferBreach(PASSIVE_COOLING_TYPE, 40) == TOO_HIGH);
+}
+
+TEST_CASE("Test3 : Temperature breach for passive coolling type high limit") {
+  REQUIRE(inferBreach(PASSIVE_COOLING_TYPE, 30) == NORMAL);
+}
+
+
+/********   Temperature breach test for high active coolling type   **********/
+TEST_CASE("Test4 : Temperature breach for high coolling type low limit") {
+  REQUIRE(inferBreach(HI_ACTIVE_COOLING_TYPE, -5) == TOO_LOW);
+}
+
+TEST_CASE("Test5 : Temperature breach for high coolling type high limit") {
+  REQUIRE(inferBreach(HI_ACTIVE_COOLING_TYPE, 50) == TOO_HIGH);
+}
+
+TEST_CASE("Test6 : Temperature breach for high coolling type high limit") {
+  REQUIRE(inferBreach(HI_ACTIVE_COOLING_TYPE, 40) == NORMAL);
+}
+
+
+/********   Temperature breach test for mid active coolling type   **********/
+TEST_CASE("Test7 : Temperature breach for mid coolling type low limit") {
+  REQUIRE(inferBreach(MED_ACTIVE_COOLING_TYPE, -5) == TOO_LOW);
+}
+
+TEST_CASE("Test8 : Temperature breach for mid coolling type high limit") {
+  REQUIRE(inferBreach(MED_ACTIVE_COOLING_TYPE, 45) == TOO_HIGH);
+}
+
+TEST_CASE("Test9 : Temperature breach for mid coolling type high limit") {
+  REQUIRE(inferBreach(MED_ACTIVE_COOLING_TYPE, 35) == NORMAL);
 }
